a933859259a9f003962c3d3c9b28871a7770f27c
mgrabulosa@castaner.com <mgrabulosa@castaner.com>
2024-10-24 13:20:02 +0200

    MGR - nova animació

--- 17820/src/app/home/home.component.ts
+++ 17820/src/app/home/home.component.ts
@@ -1,44 +1,56 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import * as $ from 'jquery';
+import { Component, AfterViewChecked, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule, BrowserAnimationsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent implements AfterViewChecked {
+  showPopup2 = false;
 
-  ngAfterViewInit() {
-    // Seleccionar los elementos con la clase zoomTarget
-    const zoomTargets = $('.zoomTarget');
-    let clickedInside = false;
+  constructor(private renderer: Renderer2) {}
 
-    // Manejar el evento de clic dentro del popup
-    zoomTargets.on('click', function() {
-      clickedInside = true;
+  togglePopup(event: MouseEvent) {
+    const target = event.currentTarget as HTMLElement;
+    const popup = target.closest('.popup');
+
+    if (popup) {
+      this.renderer.addClass(popup,'fade-out');
+      setTimeout(() => {
+        this.showPopup2 = true;
+      }, 500); 
+    } else {
+      console.log('Popup not found');
+    }
+  }
+
+  closePopup2() {
+    const popup2 = document.querySelector('.popup2');
+    if (popup2) {
+      this.renderer.removeClass(popup2,'fade-in');
+      this.renderer.addClass(popup2,'fade-out');
+      setTimeout(() => {
+        this.showPopup2 = false;
+        this.resetPopups();
+      }, 500); 
+    }
+  }
+
+  resetPopups() {
+    const popups = document.querySelectorAll('.popup');
+    popups.forEach((popup) => {
+      this.renderer.removeClass(popup,'fade-out');
     });
+  }
 
-    // Manejar el evento de mouseleave
-    zoomTargets.on('mouseleave', function() {
-      if (clickedInside) {
-        const duration = $(this).data('duration') || 600;
-        $(this).css({
-          transform: 'scale(1)',
-          transition: `transform ${duration}ms`
-        });
 
-        // Simular clic si data-closeclick es true
-        if ($(this).data('closeclick')) {
-          $(this).click();
-        }
-
-        // Resetear la bandera
-        clickedInside = false;
+  ngAfterViewChecked() {
+    if (this.showPopup2) {
+      const popup2 = document.querySelector('.popup2');
+      if (popup2) {
+        this.renderer.addClass(popup2,'show');
+        this.renderer.addClass(popup2,'fade-in');
       }
-    });
+    }
   }
 }
